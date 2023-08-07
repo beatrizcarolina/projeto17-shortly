595f97ff4b97e42a9a72bca13861d70a4ab2059b
beatrizcarolina <beatrizcarolinasr@gmail.com>
2023-08-07 17:56:21 -0300

    fix: object name JSON

--- src/repositories/user.repository.js
+++ src/repositories/user.repository.js
@@ -27,12 +27,12 @@ export async function getVisits() {
     const visits = await db.query(`
         SELECT 
             users.id, users.name,
-            COUNT(urls) AS "linkscount",
-            SUM(urls."visitscount") AS "visitscount"
+            COUNT(urls) AS "linksCount",
+            SUM(urls."visitscount") AS "visitCount"
         FROM users
         LEFT JOIN urls ON users.id = urls.userid
         GROUP BY users.id, users.name
-        ORDER BY "visitscount" DESC
+        ORDER BY "visitCount" DESC
         LIMIT 10;
     `);
 
