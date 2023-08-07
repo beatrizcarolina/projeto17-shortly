6a81b0e17f85c171e2d993c6c4a6a2efd247986c
beatrizcarolina <beatrizcarolinasr@gmail.com>
2023-08-07 09:48:34 -0300

    create user repository

---
+++ src/repositories/user.repository.js
@@ -0,0 +1,40 @@
+import { db } from "../database/database.connection.js";
+
+export async function getUser(id) {
+    const user = await db.query(`
+        SELECT 
+            users.id, 
+            users.name,
+            SUM(urls."visitCount") AS "visitCount",
+            array_agg(
+                    json_build_object(
+                    'id', urls.id, 
+                    'shortUrl', urls."shortUrl", 
+                    'url', urls.url, 
+                    'visitCount', urls."visitCount")
+                    ORDER BY urls.id
+                ) AS "shortenedUrls"
+        FROM users
+        LEFT JOIN urls ON users.id = urls.userid
+        WHERE users.id = $1
+        GROUP BY users.id
+    `, [id]);
+
+    return user.rows[0];
+};
+
+export async function getVisits() {
+    const visits = await db.query(`
+        SELECT 
+            users.id, users.name,
+            COUNT(urls) AS "linksCount",
+            SUM(urls."visitCount") AS "visitCount"
+        FROM users
+        LEFT JOIN urls ON users.id = urls.userid
+        GROUP BY users.id, users.name
+        ORDER BY "visitCount" DESC
+        LIMIT 10;
+    `);
+
+    return visits.rows;
+};
